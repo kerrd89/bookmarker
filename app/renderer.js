58de647c80df0040f822634ca519cc98735d612f
David Kerr <kerrd89@gmail.com>
2016-12-03 13:02:25 -0700

    implemented local storage and clear local storage, as well as the response validation

--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,8 @@
-const { shell } = require('electron');
+'use strict';
+require('devtron').install();
+
+const { shell, remote } = require('electron');
+const { systemPreferences } = remote;
 
 const newLinkUrl = document.querySelector('.new-link-form--url');
 const newLinkSubmit = document.querySelector('.new-link-form--submit');
@@ -6,26 +10,41 @@ const newLinkForm = document.querySelector('.new-link-form');
 const errorMessage = document.querySelector('.message');
 const linkTemplate = document.querySelector('#link-template');
 const linksSection = document.querySelector('.links');
+const clearStorageButton = document.querySelector('.controls--clear-storage')
 
 const parser = new DOMParser();
 const parseResponse = (text) => parser.parseFromString(text, 'text/html');
 const findTitle = (nodes) => nodes.querySelector('title').innerText;
 
-const addToPage = ({ title, url }) => {
-  const newLink = linkTemplate.content.cloneNode(true);
-  const titleElement = newLink.querySelector('.link--title');
-  const urlElement =  newLink.querySelector('.link--url')
-
-  titleElement.textContent = title;
-  urlElement.href = url;
-  urlElement.textContent = url;
-
-  linksSection.appendChild(newLink);
-  return { title, url };
-};
-
 newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+window.addEventListener('load', () => {
+  for (let title of Object.keys(localStorage)) {
+    addToPage({ title, url: localStorage.getItem(title) });
+  }
+});
+
+window.addEventListener('load', () => {
+  for (let title of Object.keys(localStorage)) {
+    addToPage({ title, url: localStorage.getItem(title) });
+  }
+  if (systemPreferences.isDarkMode()) {
+    document.querySelector('link').href = 'styles-dark.css';
+  }
+});
+
+linksSection.addEventListener('click', (event) => {
+  if (event.target.href) {
+    event.preventDefault();
+    shell.openExternal(event.target.href);
+  }
+});
+
+clearStorageButton.addEventListener('click', () => {
+  localStorage.clear();
+  linksSection.innerHTML = '';
 });
 
 newLinkForm.addEventListener('submit', (event) => {
@@ -39,10 +58,11 @@ newLinkForm.addEventListener('submit', (event) => {
     .then(findTitle)
     .then(title => ({ title, url }))
     .then(addToPage)
+    .then(storeLink)
     .then(clearInput)
     .catch(error => {
       console.error(error);
-      errorMessage.textContent = `There was an error fetching "${url}."`
+      errorMessage.textContent = `There was an error fetching "${url}."`;
     });
 });
 
@@ -53,6 +73,29 @@ linksSection.addEventListener('click', (event) => {
   }
 });
 
+const storeLink = ({ title, url }) => {
+  localStorage.setItem(title, url);
+  return { title, url };
+};
+
 const clearInput = () => {
   newLinkUrl.value = '';
 };
+
+const addToPage = ({ title, url }) => {
+  const newLink = linkTemplate.content.cloneNode(true);
+  const titleElement = newLink.querySelector('.link--title');
+  const urlElement =  newLink.querySelector('.link--url');
+
+  titleElement.textContent = title;
+  urlElement.href = url;
+  urlElement.textContent = url;
+
+  linksSection.appendChild(newLink);
+  return { title, url };
+};
+
+const validateResponse = (response) => {
+  if (response.ok) { return response; }
+  throw new Error(`Received a status code of ${response.status}`);
+}
